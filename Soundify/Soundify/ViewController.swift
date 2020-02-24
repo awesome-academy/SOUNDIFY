d400009a835006a6ff56e2174ff3a548f9d2b91e
anhnv-0990 <ngo.viet.anh@sun-asterisk.com>
2020-02-24 16:10:53 +0700

    Initial Project

---
+++ Soundify/Soundify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Soundify
+//
+//  Created by Viet Anh on 2/24/20.
+//  Copyright © 2020 VietAnh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
